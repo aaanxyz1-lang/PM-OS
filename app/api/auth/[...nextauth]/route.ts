a6dec76f274574ef8fdac75b3f5d32a0e6733537
aaanxyz1-lang <aaanxyz1@gmail.com>
2026-03-17 04:21:47 +0600

    Fix NextAuth & Supabase integration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,12 +4,6 @@ import { authOptions } from '@/lib/auth-options';
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
 
-const nextAuth = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-export async function GET(...args: any[]) {
-  return nextAuth(...args);
-}
-
-export async function POST(...args: any[]) {
-  return nextAuth(...args);
-}
+export { handler as GET, handler as POST };
